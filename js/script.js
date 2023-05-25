2b83d345c3342778201152e20ef3eed75e5a6738
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-25 16:34:16 +0200

    color asked

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ console.log(password);
 // chiedi al utente il nome
 const name = prompt(`inserisci il tuo nome`);
 console.log(name);
+
+// chiedi al utente il cognome
+const surname = prompt(`inserisci il tuo cognome`);
+console.log(surname);
+
+// chiedi al utente il colore preferito
+const FavCol = prompt(`Qual è il tuo colore preferito?`);
+console.log(FavCol);
+
